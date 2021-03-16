7426d102f84c7307b28bfc10b37b57d482483890
aquinas125 <quin125@naver.com>
2021-03-16 19:49:14 +0900

    test

---
+++ ios/Pods/Target Support Files/Pods-Ped_Rehab-tvOS/Pods-Ped_Rehab-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Ped_Rehab_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_Ped_Rehab_tvOS
+@end
--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -17,6 +17,7 @@ const MapScreen = () => {
     const P0 = {latitude: 37.564362, longitude: 126.977011};
     const P1 = {latitude: 37.565051, longitude: 126.978567};
     const P2 = {latitude: 37.565383, longitude: 126.976292};
+    const test = "hi";
 
     return (
       <NaverMapView style={{width: '100%', height: '100%'}}
