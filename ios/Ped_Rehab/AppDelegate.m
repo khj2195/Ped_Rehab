d26e640fb921c75768243d143af65a101504a9e8
aquinas125 <quin125@naver.com>
2021-03-26 16:15:49 +0900

    a

--- ios/Ped_Rehab/AppDelegate.m
+++ ios/Ped_Rehab/AppDelegate.m
@@ -3,6 +3,8 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <Firebase.h>
+#import <Firebasecore/FirebaseCore.h>
 
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
@@ -11,6 +13,7 @@
 #import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
 #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
 #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
+
 
 static void InitializeFlipper(UIApplication *application) {
   FlipperClient *client = [FlipperClient sharedClient];
@@ -27,6 +30,9 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+// Use Firebase library to configure APIs
+[FIRApp configure];
+
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
---
+++ ios/Pods/Target Support Files/Pods-Ped_Rehab-Ped_RehabTests/Pods-Ped_Rehab-Ped_RehabTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Ped_Rehab_Ped_RehabTests : NSObject
+@end
+@implementation PodsDummy_Pods_Ped_Rehab_Ped_RehabTests
+@end
